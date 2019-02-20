814558390e372d1a4397f273be586e673a741663
Jakub Kuszneruk <j.kuszneruk@gmail.com>
2019-02-21 00:04:34 +0100

    Introduce Django

--- autoscout_dealers.py
+++ autoscout_dealers.py
@@ -3,7 +3,7 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
-from autoscrap.spiders.autoscout_dealers import AutoscoutDealersSpider
+from crawler.spiders.autoscout_dealers import AutoscoutDealersSpider
 
 LOCALIZATIONS = {
   'it': ['Firenze', 'Bologna', 'Roma', 'Milano', 'Bergamo', 'Busto Arsizio',
